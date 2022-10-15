0f36650e16ce7b8f999d4eb458deaed75339d80e
robles153 <marcoskim153@gmail.com>
2022-10-15 15:26:26 -0300

    Implementando Enpoint

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -3,6 +3,7 @@
     public class Category : Entity
     {
        public new string Name { get; set; }
-      
+        public bool Active { get; set; } = true;
+
     }
 }
--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -8,6 +8,7 @@
         public Category Category { get; set; }
         public string Description { get; set; }
         public bool HasStock { get; set; }
-      
+        public bool Active { get; set; } = true;
+
     }
 }
