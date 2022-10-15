e642a66ea14a72e0ab8eeabae14c2163cd8f1967
robles153 <marcoskim153@gmail.com>
2022-10-15 14:56:37 -0300

    Implementando classe applicationDbContext

---
+++ Infra/Data/ApplicatioDbContext.cs
@@ -0,0 +1,31 @@
+﻿using IWANTAPP.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWANTAPP.Infra.Data
+{
+    public class ApplicatioDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicatioDbContext(DbContextOptions<ApplicatioDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>()
+                .Property(p => p.Name).IsRequired();
+            builder.Entity<Product>()
+                .Property(p => p.Description).HasMaxLength(255);
+
+            builder.Entity<Category>()
+                .Property(c => c.Name).IsRequired();
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+        {
+            configuration.Properties<string>()
+                .HaveMaxLength(100);
+        }
+
+
+    }
+}
