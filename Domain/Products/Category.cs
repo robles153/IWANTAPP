cbc1e60cca9f47620d8710cdaead1be9b1030a7d
robles153 <marcoskim153@gmail.com>
2022-10-15 14:09:11 -0300

    Criando Dominio

---
+++ Domain/Products/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace IWANTAPP.Domain.Products
+{
+    public class Category : Entity
+    {
+       public new string Name { get; set; }
+      
+    }
+}
---
+++ Domain/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace IWANTAPP.Domain.Products
+{
+    public class Product : Entity
+    {
+        
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public bool HasStock { get; set; }
+      
+    }
+}
